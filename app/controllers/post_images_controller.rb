3599c406a80f6c53c885b69c7f763f57a3916971
EC2 Default User <ec2-user@ip-172-31-3-38.ap-northeast-1.compute.internal>
2023-06-02 09:56:51 +0000

    [Finish] 4-22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,14 +7,14 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     if @post_image.save
-      redirect_to post_image_path
+      redirect_to post_images_path
     else
       render :new
     end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
